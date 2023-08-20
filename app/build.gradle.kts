11ea0d6fc6ba317e09f1047f1780328c48ecd810
Sumit Chahal <smtchahal@gmail.com>
2023-08-20 11:02:37 +0530

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.sumitchahal.tictactoe"
         minSdk = 16
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
